61ff1da5f6301bb78c5613bd337d91ebcfe4ace8
Gaspar Arimany <gaspar.arimany@gmail.com>
2022-05-19 00:08:23 -0300

    removes unnecesary api url env viariable

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,3 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 export const USER_TOKEN = 'user-token';
-export const API_URL = process.env.PROD_API_URL || 'http://localhost:3000/api';
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY!;
