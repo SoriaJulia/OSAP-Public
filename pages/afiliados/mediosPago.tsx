5234f5b509650db959f1b1d3ac4034bb98381528
SoriaJulia <soriajulias@gmail.com>
2022-06-08 23:02:50 -0300

    add medios de pago

--- pages/afiliados/mediosPago.tsx
+++ pages/afiliados/mediosPago.tsx
@@ -1,11 +1,12 @@
 import PageTitle from 'components/Base/PageTitle';
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import ContactLink from 'components/Base/ContactLink';
 import React from 'react';
-import { Buildings, Envelope } from 'phosphor-react';
-import DebitoAutomatico from '../../public/img/Deb-Aut.jpg';
+import { getSession } from 'next-auth/react';
+import { Buildings, Copy, CopySimple, DownloadSimple, Envelope, FileArrowDown } from 'phosphor-react';
+import Button from 'components/Base/Button';
 import Federacion from '../../public/img/Federacion.png';
 import Link from '../../public/img/RedLink.png';
 import PagoMisCuentas from '../../public/img/PagoMisCuentas.png';
@@ -17,85 +18,73 @@ import VisaHome from '../../public/img/VisaHome.jpg';
 
 const mediosDePago = [
   {
-    Name: 'Debito Automatico',
-    Image: DebitoAutomatico,
-    Content: (
-      <p>
-        Descargue este formulario y
-        <ContactLink
-          href="mailto:info@osap.org.ar"
-          label="envíelo a info@osap.org.ar"
-          icon={<Envelope />}
-          variant="blue"
-        />
-        <ContactLink href="" label="o acérquelo a Moreno 187" variant="blue" icon={<Buildings />} />
-      </p>
-    ),
+    Name: 'Pago mis cuentas',
+    Image: PagoMisCuentas,
+    Content: 'En la factura puede encontrar el código para hacer el pago respectivo.',
     OnClick: () => {
       window.open(
-        'http://www.osapsalud.com.ar/descargar_pdf.php?archivo=wordpress/wp-content/uploads/AUTORIZACION%20DE%20DEBITO%20AUTOMATICO%20OSAP.pdf',
+        'https://paysrv3.pagomiscuentas.com/pmctas/home.do#_ga=2.174360226.1448306210.1598373360-2092372822.1561138945',
         '_blank'
       );
     },
-  },
-  {
-    Name: 'Pago mis cuentas',
-    Image: PagoMisCuentas,
-    Content: 'En la factura puede encontrar el código para hacer el pago respectivo aquí.',
-    OnClick: () => {
-      window.open('', '_blank');
-    },
+    ActionText: 'Ir a Pago mis Cuentas',
   },
   {
     Name: 'Pronto Pago',
     Image: ProntoPago,
-    Content: 'En la factura puede encontrar el código para hacer el pago respectivo aquí.',
+    Content: 'En la factura puede encontrar el código para hacer el pago respectivo.',
     OnClick: () => {
-      window.open('', '_blank');
+      window.open('https://pagoweb.prontopago.com.ar/web.html', '_blank');
     },
+    ActionText: 'Ir a Pronto Pago',
   },
   {
     Name: 'Rapi Pago',
     Image: RapiPago,
     Content:
-      'En la factura puede encontrar el código para hacer el pago respectivo por este medio en cualquier sucursal o en la sucursal virtual.',
+      'En la factura puede encontrar el código para hacer el pago respectivo por este medio en cualquier sucursal o online.',
     OnClick: () => {
-      window.open('', '_blank');
+      window.open('https://rapipago.com.ar/rapipagoWeb/', '_blank');
     },
+    ActionText: 'Ir a Pagar Online o Buscar sucursales',
   },
   {
     Name: 'Red Link',
     Image: Link,
-    Content: 'Utilice la red link para pagar sus facturas',
+    Content: 'Utilice la red link para pagar sus facturas, desde su celular, un cajero o su homebanking de la red link',
     OnClick: () => {
-      window.open('', '_blank');
+      window.open('https://www.redlink.com.ar/link_pagos_y_cobranzas.html', '_blank');
     },
+    ActionText: 'Ir a Red Link',
   },
   {
     Name: 'Provincia Net',
     Image: ProvinciaNet,
     Content:
-      'Pague su factura en cualquier sucursal Provincia NET (ex BAPRO). O pague sin factura informando la Clave de 6 digitos o el nro. de razon social que puede encontrar en una factura anterior.',
+      'Pague su factura en cualquier sucursal Provincia NET (ex BAPRO). O sin factura informando la Clave de 6 digitos o el nro. de razon social que puede encontrar en una factura anterior.',
     OnClick: () => {
-      window.open('', '_blank');
+      window.open('https://www.provincianet.com.ar/donde-pago', '_blank');
     },
+    ActionText: 'Buscar Sucursales',
   },
   {
     Name: 'Provincian Net Pagos Online',
     Image: ProvinciaPagos,
     Content:
-      'Usted puede abonar sus servicios en línea desde cualquier dispositivo conectado a internet sin necesidad de salir de su casa. Regístrese gratis en la plataforma Provincia Net Pagos y aproveche el beneficio.',
+      'Usted puede abonar sus servicios en línea desde cualquier dispositivo conectado a internet sin necesidad de salir de su casa. Regístrese gratis y aproveche el beneficio.',
     OnClick: () => {
-      window.open('', '_blank');
+      window.open('https://pagosnet.provincianet.com.ar/pagosnet/public/application/index/landing', '_blank');
     },
+    ActionText: 'Ir a PagosNet',
   },
   {
     Name: 'Visa Home',
     Image: VisaHome,
-    Content: 'En la factura puede encontrar el código para hacer el pago respectivo aquí.',
+    Content: 'En la factura puede encontrar el código para hacer el pago respectivo.',
     OnClick: () => {
-      window.open('', '_blank');
+      window.open('https://visahome.prismamediosdepago.com/socios/login?windowId=d4f', '_blank');
     },
+    ActionText: 'Ir a Visa Home',
   },
   {
     Name: 'Federacion de Comercio e Industria San Nicolas',
@@ -103,32 +92,123 @@ const mediosDePago = [
     Content:
       'Pague con o sin factura en sucursal de la Federacion de Comercio de San Nícolas, calle Urquiza N° 32 de 8 a 17 hs.',
     OnClick: () => {
-      window.open('', '_blank');
+      window.open('https://g.page/federacionsn?share', '_blank');
     },
+    ActionText: 'Ver en el Mapa',
   },
 ];
 
-const MediosPago: NextPage = () => {
+const MediosPago: NextPage<{ agentId: string }> = ({ agentId }) => {
   return (
     <div className="text-left">
       <Head>
         <title>Medios de Pago - OSAP</title>
       </Head>
       <PageTitle title="Medios de Pago" />
-      <div className="flex flex-wrap gap-3">
-        {mediosDePago.map((medio) => {
-          return (
-            <div className="flex w-[45%] items-center justify-evenly rounded bg-white p-3 text-left drop-shadow">
-              <button className="mr-3  flex-shrink-0 p-1" onClick={medio.OnClick}>
-                <Image src={medio.Image} className="object-scale-down" width={200} height={120} />
-              </button>
-              {medio.Content}
+      <div className="flex flex-wrap gap-8">
+        <article className="pb-10">
+          <h3 className="mb-3  text-2xl text-orange-700">Pago Online</h3>
+          <div className="mb-10 flex items-center gap-4 text-lg">
+            <div className=" flex w-8/12 flex-col">
+              Ingrese a nuestro sistema de pago y pague con cualquier Tarjeta de Debito
+              <span className="mt-1 flex text-sm">
+                *Agente de Cuenta: {agentId}
+                <button
+                  onClick={() => {
+                    navigator.clipboard.writeText(agentId);
+                  }}
+                  title="Copiar"
+                >
+                  <CopySimple
+                    className="mx-1 rounded-full p-1 text-blue-600 hover:bg-white"
+                    size={22}
+                    weight="regular"
+                  />
+                </button>
+              </span>
             </div>
-          );
-        })}
+            <Button
+              label="Ingresar"
+              variant="yellowOutlined"
+              onClick={() =>
+                window.open(`https://osapjubilados.prontopago.com.ar:4545/?serviceid=17944&Param1=${agentId}`, '_blank')
+              }
+            />
+          </div>
+        </article>
+        <article className="pb-10">
+          <h3 className="mb-3 text-2xl text-orange-700">Debito Automatico</h3>
+          Para adherirse descargue el siguiente formulario:
+          <button
+            onClick={() => {
+              window.open(
+                'http://www.osapsalud.com.ar/descargar_pdf.php?archivo=wordpress/wp-content/uploads/AUTORIZACION%20DE%20DEBITO%20AUTOMATICO%20OSAP.pdf',
+                '_blank'
+              );
+            }}
+            title="Descargar formulario"
+          >
+            <FileArrowDown
+              className="m-1 inline rounded-full p-1 text-blue-500 hover:bg-white hover:text-blue-700"
+              weight="regular"
+              size={28}
+            />
+          </button>
+          <div className="flex flex-wrap gap-1">
+            y envíelo a
+            <ContactLink
+              href="mailto:info@osap.org.ar"
+              label="info@osap.org.ar"
+              icon={<Envelope />}
+              variant="blue"
+              inline
+            />
+            o acérquelo a nuesta Sede
+            <ContactLink href="" label="Moreno 187" variant="blue" icon={<Buildings />} inline />
+          </div>
+        </article>
       </div>
+      <article>
+        <h3 className="mb-4 text-2xl text-orange-700">Otros medios de pago</h3>
+        <div className="flex flex-wrap gap-3">
+          {mediosDePago.map((medio) => {
+            return (
+              <div
+                className="flex max-w-lg flex-col items-center justify-evenly rounded bg-white p-3 text-left drop-shadow sm:flex-row lg:w-[48%] lg:max-w-full"
+                key={medio.Name}
+              >
+                <button
+                  className="mr-3 mb-2 flex flex-shrink-0 flex-col items-center gap-2 p-1 text-sm text-slate-600 underline sm:mb-0"
+                  onClick={medio.OnClick}
+                >
+                  <Image src={medio.Image} className="object-scale-down" width={200} height={120} />
+                  {medio.ActionText}
+                </button>
+                {medio.Content}
+              </div>
+            );
+          })}
+        </div>
+      </article>
     </div>
   );
 };
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req });
 
+  if (!session || session.status === 'unauthenicated') {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+
+  const agentId = session.user?.agentId;
+
+  return {
+    props: { agentId },
+  };
+};
 export default MediosPago;
