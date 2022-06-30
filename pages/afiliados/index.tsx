ceb7df05bc071b0680618f168ff9a0a7873b8492
SoriaJulia <soriajulias@gmail.com>
2022-06-30 11:32:08 -0300

    test error coseguros

--- pages/afiliados/index.tsx
+++ pages/afiliados/index.tsx
@@ -102,8 +102,14 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   let credenciales = [];
   let facturas = [];
   let autorizaciones = [];
-  let coseguros = [];
 
+  // try {
+  //   coseguros = await nextFetch(`afiliado/${agentId}/coseguro`, {
+  //     headers: { Cookie: req.headers.cookie || '' },
+  //   });
+  // } catch (err) {
+  //   console.error(err);
+  // }
   try {
     facturas = await nextFetch(`afiliado/${agentId}/factura`, {
       headers: { Cookie: req.headers.cookie || '' },
@@ -125,15 +131,8 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   } catch (err) {
     console.error(err);
   }
-  try {
-    coseguros = await nextFetch(`afiliado/${agentId}/coseguro`, {
-      headers: { Cookie: req.headers.cookie || '' },
-    });
-  } catch (err) {
-    console.error(err);
-  }
   return {
-    props: { facturas, credenciales, autorizaciones, agentId, coseguros, convenio },
+    props: { facturas, credenciales, autorizaciones, agentId, coseguros: [], convenio },
   };
 };
 
