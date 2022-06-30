62bb83b4a0a4d08843661284e67517e922d25116
SoriaJulia <soriajulias@gmail.com>
2022-06-30 12:11:04 -0300

    logs test

--- lib/utils.ts
+++ lib/utils.ts
@@ -40,13 +40,14 @@ export const parseJSONResponse = (actionName: string, xml: string) => {
 
 export const nextFetch = async (url: string, options?: RequestInit) => {
   const result = await fetch(`${NEXT_URL}/${url}`, options);
+  console.log('result', result);
+  console.log(result.statusText);
+  console.log('is ok', result.ok);
 
   if (result.ok) {
     const data = await result.json();
     return data;
   }
-  console.error(result.statusText);
-  console.log('result', result);
 };
 
 export const changeTextInput =
