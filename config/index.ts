476836fc9aadb70bea9cd9073261cfb281f844d4
SoriaJulia <soriajulias@gmail.com>
2022-08-12 20:07:13 -0300

    fix nex public urls

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
-export const GECROS_API_URL = process.env.GECROS_API_URL || '';
-export const OSAP_API_URL = process.env.OSAP_API_URL || '';
+export const GECROS_API_URL = process.env.NEXT_PUBLIC_GECROS_API_URL || '';
+export const OSAP_API_URL = process.env.NEXT_PUBLIC_OSAP_API_URL || '';
 export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000/api';
