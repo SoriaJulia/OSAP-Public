02a24488aaca2e46347564f77426287215b3e376
Julia Soria <soriajulias@gmail.com>
2023-08-24 11:22:16 -0300

    security cleanup

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,3 @@
-export const GECROS_API_URL =
-  process.env.NEXT_PUBLIC_GECROS_API_URL ||
-  process.env.GECROS_API_URL ||
-  'http://190.228.161.157:81/wsgecrosnet/wsgecrosnet.asmx';
-export const OSAP_API_URL =
-  process.env.NEXT_PUBLIC_OSAP_API_URL || process.env.OSAP_API_URL || 'https://ws.osap.com.ar:444';
-export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:3000/api';
+export const GECROS_API_URL = process.env.NEXT_PUBLIC_GECROS_API_URL || process.env.GECROS_API_URL;
+export const OSAP_API_URL = process.env.NEXT_PUBLIC_OSAP_API_URL || process.env.OSAP_API_URL;
+export const NEXT_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
--- lib/constants.ts
+++ lib/constants.ts
@@ -7,5 +7,5 @@ export const NETWORK_ERROR =
   'Hubo un error al conectarse con el servidor. Revise su conexión a internet e intente nuevamente';
 export const DEFAULT_CACHE_TIME = 300000;
 export const DEFAULT_STALE_TIME = 400000;
-export const SANITY_API_PROJECT_ID = process.env.NEXT_PUBLIC_SANITY_API_PROJECT_ID! || 'p6yd2y5a';
+export const SANITY_API_PROJECT_ID = process.env.NEXT_PUBLIC_SANITY_API_PROJECT_ID!;
 export const SANITY_DATASET_NAME = process.env.NEXT_PUBLIC_SANITY_API_DATASET!;
